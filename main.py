4e7b2715eab47176adc1944749ddbabeeb466002
Anasooya <anasooyadutta@gmail.com>
2023-04-24 08:49:42 +0530

    first commit

--- main.py
+++ main.py
@@ -1,90 +1,106 @@
 import os
-import shutil
-from flask import Flask, render_template, request, redirect, url_for
 import zipfile
-import io
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
-from pdfminer.pdfpage import PDFPage
-import datetime
+from tkinter import *
+from tkinter import filedialog
 
-app = Flask(__name__)
+class FileZipper:
+    def __init__(self, master):
+        self.master = master
+        master.title("File Zipper")
+        self.file_paths = []
+        self.current_page = 0
 
-# Defining a function to read PDF file
-def read_pdf_file(file_path):
-    resource_manager = PDFResourceManager()
-    buffer = io.StringIO()
-    laparams = LAParams()
-    device = TextConverter(resource_manager, buffer, laparams=laparams)
-    with open(file_path, 'rb') as fp:
-        interpreter = PDFPageInterpreter(resource_manager, device)
+        # Create file selection button
+        self.select_button = Button(master, text="Select Files", command=self.select_files)
+        self.select_button.pack(pady=10)
 
-        # Extract only the first page of the PDF
-        for page in PDFPage.get_pages(fp, check_extractable=True, maxpages=1):
-            interpreter.process_page(page)
-        text = buffer.getvalue()
+        # Create file deletion button
+        self.delete_button = Button(master, text="Delete Selected File", command=self.delete_file)
+        self.delete_button.pack(pady=10)
 
-        # Extract only the first paragraph of the first page
-        first_para = text.split('\\\\\\\\n\\\\\\\\n')[0]
-    device.close()
-    buffer.close()
-    return first_para
+        # Create zip button
+        self.zip_button = Button(master, text="Zip Files", command=self.zip_files)
+        self.zip_button.pack(pady=10)
 
-def extract_information(file_path, search_term=None):
-    # Create an empty list to store information extracted from files
-    information = []
-    # Loop through all files in the directory
-    for filename in os.listdir(file_path):
-        # Check if the file is a PDF file
-        if filename.endswith('.pdf'):
-            # Read the first paragraph of the PDF file
-            first_para = read_pdf_file(os.path.join(file_path, filename))
-            # Get the creation date of the file
-            creation_date = datetime.datetime.fromtimestamp(os.path.getctime(os.path.join(file_path, filename)))
-            # Add the information to the list
-            information.append((filename, creation_date, first_para))
-    # Filter the information based on the search term
-    if search_term:
-        information = [item for item in information if search_term.lower() in item[2].lower()]
-    return information
+        # Create navigation buttons
+        self.back_button = Button(master, text="<<", command=self.previous_page)
+        self.back_button.pack(side=LEFT, padx=10)
+        self.forward_button = Button(master, text=">>", command=self.next_page)
+        self.forward_button.pack(side=RIGHT, padx=10)
+
+        # Create file listbox
+        self.file_listbox = Listbox(master, width=50, height=10, selectmode=SINGLE)
+        self.file_listbox.pack()
+
+        # Create scrollbar for file listbox
+        self.scrollbar = Scrollbar(master)
+        self.scrollbar.pack(side=RIGHT, fill=Y)
+        self.file_listbox.config(yscrollcommand=self.scrollbar.set)
+        self.scrollbar.config(command=self.file_listbox.yview)
+
+    def select_files(self):
+        file_types = [("PDF Files", "*.pdf"), ("JPEG Files", "*.jpg")]
+        files = filedialog.askopenfilenames(filetypes=file_types)
+        for file in files:
+            if file.endswith(".pdf") or file.endswith(".jpg"):
+                self.file_paths.append(file)
+                self.file_listbox.insert(END, file)
+
+    def delete_file(self):
+        selected = self.file_listbox.curselection()
+        if selected:
+            index = int(selected[0])
+            self.file_paths.pop(index)
+            self.file_listbox.delete(selected)
+
+    def zip_files(self):
+        if not self.file_paths:
+            messagebox.showwarning("No Files Selected", "Please select files to zip.")
+        else:
+            zip_file = filedialog.asksaveasfilename(defaultextension=".zip")
+            with zipfile.ZipFile(zip_file, "w") as zip:
+                for file in self.file_paths:
+                    zip.write(file, os.path.basename(file))
+
+    def next_page(self):
+        if self.current_page < len(self.file_paths) - 1:
+            self.current_page += 1
+            self.file_listbox.select_clear(0, END)
+            self.file_listbox.select_set(self.current_page)
+            self.file_listbox.activate(self.current_page)
+
+    def previous_page(self):
+        if self.current_page > 0:
+            self.current_page -= 1
+            self.file_listbox.select_clear(0, END)
+            self.file_listbox.select_set(self.current_page)
+            self.file_listbox.activate(self.current_page)
+
+root = Tk()
+file_zipper = FileZipper(root)
+root.mainloop()
 
 
 
 
 
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    # Get the search term from the query parameter
-    search_term = request.args.get('search')
-    # Check if a file has been uploaded
-    if request.method == 'POST':
-        # Get the uploaded file
-        uploaded_file = request.files['file']
-        # Extract the contents of the zip file
-        with zipfile.ZipFile(io.BytesIO(uploaded_file.read()), 'r') as zip_ref:
-            # Get the directory containing the files
-            file_path = zip_ref.namelist()[0]
-            # Extract the files to a temporary directory
-            zip_ref.extractall('/tmp')
-            # Extract information from the files
-            information = extract_information('/tmp/' + file_path, search_term)
-            # Render the HTML template with the information and search term
-            return render_template('index.html', information=information, search_term=search_term)
-    # Check if the clear button has been clicked
-    elif request.method == 'GET' and 'clear' in request.args:
-        # Clear all files from the temporary directory
-        shutil.rmtree('/tmp')
-        os.mkdir('/tmp')
-        # Render the HTML template for uploading a file
-        return render_template('upload.html')
-    # Render the HTML template for uploading a file
-    return render_template('upload.html')
-    # Render the HTML template for uploading a file
-    return render_template('upload.html')
-if __name__ == '__main__':
-    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
